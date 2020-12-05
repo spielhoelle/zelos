25c2779ef969c685e307a07c35cd623beaf89452
Tommy <spielhoelle@posteo.net>
2020-12-05 07:50:36 +0100

    feat: redirect if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
   def index
     @title = "Zelos Invoice"
     @entries = Entry.all
+    if user_signed_in? 
+      redirect_to admin_entries_path
+    end
   end
 
 end
